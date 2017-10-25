055d3d5f7b3bfee12647b9d26a2cd43427eb1fc2
mjlivingston40 <matt.j.livingston.40@gmail.com>
2017-10-24 22:40:47 -0500

    mac os venv

--- app.py
+++ app.py
@@ -2,13 +2,27 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# basic test of react
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
+# basic test of filtering bootstrap 3 panels
+
+
 @app.route("/grid")
 def grid():
     return render_template('grid.html')
+
+# extension of grid
+
+
+@app.route("/grid2")
+def grid():
+    return render_template('grid2.html')
+
 
 if __name__ == "__main__":
     app.run()
